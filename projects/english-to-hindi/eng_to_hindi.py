9fedb6fb1f384fbb1647a438cd02465943418785
Suman Das <sumandas@SUMANs-MacBook-Pro-2.local>
2022-07-24 22:02:32 +0530

    added translator

--- projects/english-to-hindi/eng_to_hindi.py
+++ projects/english-to-hindi/eng_to_hindi.py
@@ -6,13 +6,13 @@ from converter import EngtoHindi
 # and background set for grey
 master = Tk()
 master.title('English to Hindi Translator')
-master.geometry("1200x200")
+master.geometry("1540x200")
 master.configure(bg='#AED6F1')
 
 # Variable Classes in tkinter
 result = StringVar()
 
-e = Entry(master, width=100)
+e = Entry(master, width=90, font=("Arial", 25))
 e.grid(row=0, column=1)
 
 # Creating label for each information
@@ -36,7 +36,7 @@ def eng_to_hindi():
 
 # creating a button using the widget
 # Button that will call the submit function
-b = Button(master, text="Translate", command=eng_to_hindi, bg="#1A5276")
+b = Button(master, text="Translate", command=eng_to_hindi, bg="#1A5276",font=("Arial", 13))
 b.grid(row=0, column=2, columnspan=2, rowspan=2, padx=2, pady=2, )
 
 mainloop()
